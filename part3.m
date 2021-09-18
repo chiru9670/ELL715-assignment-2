028f4edfb2c21235f2dcdd681d46a55e2874458e
Anirudh Panigrahi <chiru9670@gmail.com>
2021-09-18 21:50:59 +0530

    Add close all, clear variables to part3.m Remove canny edge detection on frame1

--- part3.m
+++ part3.m
@@ -1,4 +1,7 @@
-v = VideoReader('bones.mp4');
+clear variables
+close all
+
+v = VideoReader('images/bones.mp4');
 frame1 = rgb2gray(readFrame(v));
 frame2 = rgb2gray(readFrame(v));
 
@@ -7,6 +10,3 @@ diff = frame1 - frame2;
 imwrite(frame1, 'frame1.png');
 imwrite(frame2, 'frame2.png');
 imwrite(diff, 'diff_of_frames.png');
-
-edge_frame1 = canny_edge(frame1);
-boosted_edges = (edge_frame1 + 0.9 * diff);
